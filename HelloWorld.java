20aba73e38efbb3171c7da563ce8c2a2f8dcf807
Adrien Burgess <a.burke.burgess@gmail.com>
2025-02-08 20:37:08 -0400

    test webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Second World github scm polling via webhook");
+        System.out.println("Hello Second World github scm polling via webhook 2");
     }
 }
